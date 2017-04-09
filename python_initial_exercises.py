7dccd34b1ead12620f1d9ac072651b8fa308d711
Muhammad Affan Khan <maffan@folio3.com>
2017-04-09 19:35:54 +0500

    refactoring

--- python_initial_exercises.py
+++ python_initial_exercises.py
@@ -1,40 +1,40 @@
-print("Hello World");
-print("This is not my first python script :D");
+print "Hello World"
+print "This is not my first python script :D"
 
-print("Summary till ex.10");
+print "Summary till ex.10"
 
 # this is how we comment the code in python
 
-name = "Affan";
+name = "Affan"
 
 # this is how to print variables in python
 print "My name is %s" % name
 
-# some of the descisions making
-print 5 < 7;
-print 5 <= 5;
+# some of the decisions making
+print 5 < 7
+print 5 <= 5
 
-age = 17;
+age = 17
 
-if (age < 18):
+if age < 18:
     print "your too young to join the party"
 
 # new chap: numbers and maths
 
 cars = 45
-spaceInCars = 4.0
+spaceInCars = 5.0
 passengers = 76
 drivers = 34
 
-if (drivers < cars):
+if drivers < cars:
     print "Not all cars will be driven"
 
 # seating arrangements will be like
 
 carsDriven = drivers
-passengersCanBeTaken = carsDriven*spaceInCars - drivers;
+passengersCanBeTaken = carsDriven*spaceInCars - drivers
 print passengersCanBeTaken
-if(passengersCanBeTaken > (passengers+drivers)):
+if passengersCanBeTaken > (passengers+drivers):
     print "All passengers can travel"
 else:
     print "Sorry insufficient cars"
