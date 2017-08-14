65bd2b2d45f3427a74dd182dca0ce9b7cb26ec2f
Muhammad Affan Khan <maffan@folio3.com>
2017-08-14 20:12:18 +0500

    initial commit

---
+++ loops.py
@@ -0,0 +1,21 @@
+# practicing loops
+
+hairs = ['brown', 'blond', 'red']
+eyes = ['brown', 'blue', 'green']
+weights = [1, 2, 3, 4]
+elements = []
+# for loop practice
+for i in hairs:
+    elements.append(i)
+
+for i in eyes:
+    elements.append(i)
+for i in weights:
+    elements.append(i)
+
+for i in elements:
+    print(i)
+print(elements.__len__())
+
+
+# while loop practice
--- python_initial_exercises.py
+++ python_initial_exercises.py
@@ -1,40 +1,17 @@
-print "Hello World"
-print "This is not my first python script :D"
+# name = 'Affan'
+# print('New Python Calling my name: ' + name)
 
-print "Summary till ex.10"
-
-# this is how we comment the code in python
-
-name = "Affan"
-
-# this is how to print variables in python
-print "My name is %s" % name
-
-# some of the decisions making
-print 5 < 7
-print 5 <= 5
-
-age = 17
-
-if age < 18:
-    print "your too young to join the party"
-
-# new chap: numbers and maths
-
-cars = 45
-spaceInCars = 5.0
-passengers = 76
-drivers = 34
-
-if drivers < cars:
-    print "Not all cars will be driven"
-
-# seating arrangements will be like
-
-carsDriven = drivers
-passengersCanBeTaken = carsDriven*spaceInCars - drivers
-print passengersCanBeTaken
-if passengersCanBeTaken > (passengers+drivers):
-    print "All passengers can travel"
+age = 1
+minimumAgeLimitForBoys = 18
+minimumAgeLimitForGirls = 16
+if age > minimumAgeLimitForBoys:
+    print('You are in...')
 else:
-    print "Sorry insufficient cars"
+    print('Sorry this time, but we\'ll be happy to see you after ' + str (minimumAgeLimitForBoys - age) + ' years')
+
+if True == False:
+    print('True')
+elif False == True:
+    print('False')
+else:
+    print('Hi')
